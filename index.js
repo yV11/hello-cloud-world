83823b2826645c7aa50e766fd70b2002456c3a5d
Yanick <yV11@users.noreply.github.com>
2021-03-02 14:28:04 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+
 app.get('/', (req, res) => {
   res.send('Hello (Cloud) World!');
 });
+
 
 app.listen(port, () => {
   console.log('[Info] web-app has been started.');
